e9163908c07050ca4828cb399c02cb11bfa356c1
JeongShin <sjeong1127@gmail.com>
2022-12-27 20:49:42 +0900

    chore: add basic concept of global components

--- lib/typescript/components/BottomSheetPanGestureWrapper.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-declare const BottomSheetPanHandlerWrapper: React.FC<BottomSheetPanHandlerWrapperProps>;
-type BottomSheetPanHandlerWrapperProps = {
-    children?: React.ReactElement;
-};
-export default BottomSheetPanHandlerWrapper;
-//# sourceMappingURL=BottomSheetPanGestureWrapper.d.ts.map
--- lib/typescript/hooks/useBottomSheetContext.d.ts
+++
@@ -1,3 +0,0 @@
-declare const useBottomSheetContext: () => import("../context/BottomSheetContext").BottomSheetContextType;
-export default useBottomSheetContext;
-//# sourceMappingURL=useBottomSheetContext.d.ts.map
