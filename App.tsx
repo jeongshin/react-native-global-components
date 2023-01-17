700d6051103b8026d7c0fd200e75d0b0516daa4e
Jerry <sjeong1127@gmail.com>
2023-01-17 23:47:21 +0900

    feat: add storybook settings

--- App.tsx
+++ App.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { createPopup } from 'react-native-global-components';
+import StorybookUIRoot from './storybook';
 
 export default function App() {
-  console.log(createPopup);
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StorybookUIRoot />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
