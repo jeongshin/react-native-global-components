285952ce307e0430edb76d47f6011d9bbe7941d1
Jerry <sjeong1127@gmail.com>
2023-01-17 23:13:20 +0900

    chore: renewal expo & build settings

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  // console.log(ud);
   return (
     <View style={styles.container}>
+      {/* <Overlay /> */}
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+
+const root = path.resolve(__dirname, './packages');
+const rootPak = JSON.parse(
+  fs.readFileSync(path.join(root, 'package.json'), 'utf8'),
+);
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            // For development, we want to alias the library to the source
+            [rootPak.name]: path.join(root, rootPak['react-native']),
+            // [appPak.name]: path.join(app, appPak['react-native']),
+          },
+        },
+      ],
+    ],
   };
 };
