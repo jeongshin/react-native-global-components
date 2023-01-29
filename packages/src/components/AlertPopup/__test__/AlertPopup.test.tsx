f4bae048567093ff025d4c86cd82f1010419d685
Jerry <sjeong1127@gmail.com>
2023-01-29 14:47:32 +0900

    feat: add alert popup test code

--- packages/src/components/AlertPopup/__test__/AlertPopup.test.tsx
+++ packages/src/components/AlertPopup/__test__/AlertPopup.test.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { act, cleanup, render, RenderAPI } from '@testing-library/react-native';
+import {
+  act,
+  fireEvent,
+  render,
+  RenderAPI,
+  waitFor,
+} from '@testing-library/react-native';
 import { createPopup } from '../../..';
 import AlertPopup from '../AlertPopup';
 
 let renderAPI: RenderAPI;
 
 describe('[AlertPopup]', () => {
-  // afterEach(() => cleanup());
-
   it('should fire onPress on press option', async () => {
     const testID = 'Option1';
     const text = 'Good!!';
 
     let pressedText: string | undefined;
 
-    const { Portal, show, clear } = createPopup(AlertPopup);
-
-    clear();
+    const { Portal, show } = createPopup(AlertPopup);
 
     renderAPI = render(<Portal />);
 
@@ -24,19 +26,25 @@ describe('[AlertPopup]', () => {
       pressedText = text;
     };
 
-    show({
-      title: `Hi I'm Horizontal Popup`,
-      message: 'select how do you feel today',
-      options: [
-        { text, onPress, testID },
-        { text: 'placeholder', onPress },
-      ],
+    act(() => {
+      show({
+        title: `Hi I'm Horizontal Popup`,
+        message: 'select how do you feel today',
+        options: [
+          { text, onPress, testID },
+          { text: 'placeholder', onPress },
+        ],
+      });
     });
 
-    const option = await renderAPI.findByTestId(testID);
+    const option = renderAPI.getByTestId(testID);
 
-    option.props.onClick && option.props.onClick();
+    fireEvent.press(option);
 
     expect(pressedText).toEqual(text);
+
+    await waitFor(async () => {
+      expect(renderAPI.toJSON()).toBeFalsy();
+    });
   });
 });
