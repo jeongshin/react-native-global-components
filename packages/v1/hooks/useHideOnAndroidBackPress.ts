f2f366586b9b56dc4bea487b96a93bcae36289e7
Jerry <sjeong1127@gmail.com>
2023-05-05 14:18:07 +0900

    feat: [useHideOnAndroidBackPress] add backHandler option

--- packages/v1/hooks/useHideOnAndroidBackPress.ts
+++ packages/v1/hooks/useHideOnAndroidBackPress.ts
@@ -2,21 +2,27 @@ import { useEffect, useRef } from 'react';
 import { BackHandler, Platform } from 'react-native';
 import { usePopupContext } from '../context';
 
+interface Params {
+  enabled?: boolean;
+  backHandler?: () => void;
+}
 /**
  * use global component action on android hardware back press event
  * @param {boolean} p.enabled if true hide current global component else nothing happens
  */
-const useHideOnAndroidBackPress = ({ enabled }: { enabled: boolean }) => {
+const useHideOnAndroidBackPress = ({ enabled = true, backHandler }: Params) => {
   const { hide } = usePopupContext();
 
   const platform = useRef(Platform.OS).current;
 
   useEffect(() => {
     if (platform !== 'android') return;
+
     const subscription = BackHandler.addEventListener(
       'hardwareBackPress',
       () => {
         enabled && hide();
+        backHandler && backHandler();
         return true;
       },
     );
