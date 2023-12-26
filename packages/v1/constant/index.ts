e8e10678dbe3073fa31ee9a7bc9f5e747bf50f0b
Huckleberry <huckleberry@wrtn.io>
2023-12-26 12:11:34 +0900

    fix: ignore reduce motion settings for default

--- packages/v1/constant/index.ts
+++ packages/v1/constant/index.ts
@@ -1,6 +1,11 @@
-import { Easing, WithTimingConfig } from 'react-native-reanimated';
+import {
+  Easing,
+  ReduceMotion,
+  WithTimingConfig,
+} from 'react-native-reanimated';
 
 export const DEFAULT_WITH_TIMING_CONFIG: WithTimingConfig = {
   duration: 300,
   easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+  reduceMotion: ReduceMotion.Never,
 };
